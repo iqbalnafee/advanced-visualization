f8e8472a85ee6f8feb43a9520adc30ab4ee89708
Nafees Iqbal <33998031+iqbalnafee@users.noreply.github.com>
2023-11-25 23:09:18 +0100

    exercise data shown

--- d3.js/js/app.js
+++ d3.js/js/app.js
@@ -1,12 +1,106 @@
 
 
-d3.json('../d3.js/json/json_data.json').then(
+d3.json('../d3.js/json/source_code_data.json').then(
+//d3.json('../d3.js/json/json_data.json').then(
   d => drawTree(d)
+  //d => drawTreeSample(d)
 );
 
+function groupAttributesByFilename(data) {
+  const groupedData = {};
+
+  data.forEach(item => {
+    const { filename, ...attributes } = item;
+    groupedData[filename] = {
+      filename,
+      children: [attributes.developer]
+    }; 
+  });
+
+  return  [{
+    children: Object.values(groupedData).map(entry => ({
+      filename: entry.filename,
+      children: entry.children
+    }))
+  }];
+
+  // return {
+  //   data : Object.values(children) 
+  // }
+}
+
+
 function drawTree(treeData) {
+  treeData = groupAttributesByFilename(treeData.data);
 
+  // Displaying the result
+  console.log(treeData);
+  
+  let colors = ["yellow","red","purple","blue","green","orange"];
+  let radius = 10;
+  // set the dimensions and margins of the diagram
+  const margin = {top: 20, right: 90, bottom: 30, left: 90},
+  width  = 660 - margin.left - margin.right,
+  height = 500 - margin.top - margin.bottom;
 
+  // declares a tree layout and assigns the size
+  const treemap = d3.tree().size([height, width]);
+
+  //  assigns the data to a hierarchy using parent-child relationships
+  let nodes = d3.hierarchy(treeData[0], d => d.children);
+
+  // maps the node data to the tree layout
+  nodes = treemap(nodes);
+
+  // append the svg object to the body of the page
+  // appends a 'group' element to 'svg'
+  // moves the 'group' element to the top left margin
+  const svg = d3.select("body").append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom),
+  g = svg.append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
+
+  // adds the links between the nodes
+  const link = g.selectAll(".link")
+  .data( nodes.descendants().slice(1))
+  .enter().append("path")
+  .attr("class", "link")
+  .style("stroke", (d,i) => colors[i%colors.length])
+  .attr("d", d => {
+  return "M" + d.y + "," + d.x
+    + "C" + (d.y + d.parent.y) / 2 + "," + d.x
+    + " " + (d.y + d.parent.y) / 2 + "," + d.parent.x
+    + " " + d.parent.y + "," + d.parent.x;
+  });
+
+  // adds each node as a group
+  const node = g.selectAll(".node")
+  .data(nodes.descendants())
+  .enter().append("g")
+  .attr("class", d => "node" + (d.children ? " node--internal" : " node--leaf"))
+  .attr("transform", d => "translate(" + d.y + "," + d.x + ")");
+
+  // adds the circle to the node
+  node.append("circle")
+  .attr("r", radius)
+  .style("stroke", (d,i) => colors[i%colors.length])
+  .style("fill", (d,i) => colors[i%colors.length]);
+
+  // adds the text to the node
+  node.append("text")
+  .attr("dy", ".35em")
+  .attr("x", d => d.children ? (radius + 5) * -1 : radius + 5)
+  .attr("y", d => d.children && d.depth !== 0 ? -(radius + 5) : d)
+  .style("text-anchor", d => d.children ? "end" : "start")
+  .text(d => d.data.filename);
+}
+
+function drawTreeSample(treeData) {
+
+  console.log(treeData);
+  
   let colors = ["yellow","red","purple","blue","green","orange"];
   let radius = 10;
   // set the dimensions and margins of the diagram
